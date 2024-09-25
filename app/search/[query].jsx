7ae266f6be80548bafd60ddc9d27479886a1cf37
Rashadul-hub <rashadulislamhimu@gmail.com>
2024-09-25 18:57:12 +0600

    3.20 Added indexes for video Databases

--- app/search/[query].jsx
+++ app/search/[query].jsx
@@ -1,11 +1,73 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, FlatList, Image, RefreshControl } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'  
+import { images } from '../../constants'
+import SearchInput from '../../components/SearchInput'
+import Trending from '../../components/Trending'
+import EmptyState from '../../components/EmptyState'
+import { useState, useEffect } from 'react'
+import useAppwrite from '../../lib/useAppwrite'
+import { getAllPosts, getLatestPosts, searchPosts } from '../../lib/appwrite'
+import VideoCard from '../../components/VideoCard'
+import { useLocalSearchParams } from 'expo-router'
+
 
 const Search = () => {
+
+  const {query} = useLocalSearchParams()
+  const { data: posts, refetch } = useAppwrite(() => searchPosts(query));
+ 
+  useEffect(() => {
+    refetch()
+  }, [query])
+  
+
+  console.log(posts);
+  
   return (
-    <View>
-      <Text>Search</Text>
-    </View>
+    <SafeAreaView className="bg-primary border-2   h-full">
+
+     {/* Normal Posts  */}
+      <FlatList
+        data={posts}
+         keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => (
+
+          <VideoCard
+            video={item}
+          />
+
+        )}
+        ListHeaderComponent={() => (
+          <View className="my-6 px-4">
+            
+            <Text className="font-pmedium text-sm text-gray-100">
+                Search Results
+            </Text>
+             <Text className="text-2xl font-psemibold text-white">
+                {query}
+             </Text>
+
+             <View className="mt-6 mb-8">
+
+              {/* Search Bar */}
+             <SearchInput initialQuery={query}/>
+
+             </View>
+           
+          </View>
+        )}
+
+        ListEmptyComponent={() => (
+          <EmptyState
+            title= "No Videos"
+            subtitle= "No videos found in this keyword"
+          />
+        )}
+      
+      />
+
+    </SafeAreaView>
+       
   )
 }
 
